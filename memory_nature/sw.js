5cdadf00bfaf054ddb99f17df434cb2e31ee6396
greg <grzebag@gmail.com>
2026-06-03 12:12:32 +0200

    feat: service worker for PWA offline support

---
+++ memory_nature/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'nature-memory-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './favicon.png',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
